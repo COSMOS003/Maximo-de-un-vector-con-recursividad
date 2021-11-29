09632985b50644d9a50801f13b8702ec890dfe54
COSMOS003 <CeliaNGMenacho83@gmail.com>
2021-11-29 10:46:56 -0400

    cel

---
+++ src/Ejercicios.java
@@ -0,0 +1,96 @@
+public class Ejercicios {
+    /*
+    Invertir una cadena de texto "Recursividad" = "dadivisruceR"
+     */
+    private static String invertirCadena(String cadena, int indice){
+        String inversa;
+
+        if(indice == 0){
+            //caso base
+            inversa = String.valueOf(cadena.charAt(indice));
+
+        }else{//llamada recursiva
+            inversa = String.valueOf(cadena.charAt(indice)) + invertirCadena(cadena, indice -1);
+
+
+        }
+
+
+        return inversa;
+    }
+
+    /*
+   Metodo auxiliar que se llama al metodo recursivo
+    */
+    public static String invertirCadena(String cadena){
+        return invertirCadena(cadena, cadena.length() - 1);
+    }
+
+    /*
+    Metodo recursivo para comparar dos vectores de enteros elementos por elementos
+    vector1 = {5, 6, 2}
+    vector2 = {2, 9, 2}
+    diferen = 3 -3 + 0 = 6
+     */
+    private static int compararVectores(int[] vector1, int[] vector2, int indice){
+        int sumaDiferencias;
+
+        if (indice == vector1.length - 1){//caso base
+            sumaDiferencias = Math.abs(vector1[indice] - vector2[indice]);
+
+        }else{
+            sumaDiferencias = Math.abs(vector1[indice] - vector2[indice]) + compararVectores(vector1, vector2, indice +1);
+        }
+
+        return sumaDiferencias;
+
+    }
+
+    /*
+    Metodo auxiliar que realizara llamada al metodo recursivo
+     */
+    public static boolean compararVectores(int[] vector1, int[] vector2){
+        return compararVectores(vector1, vector2, 0 ) == 0;
+    }
+
+     /*
+    Metodo recursivo para calcular el maximo valor de un vector
+
+
+    vector = {2, 4, 6, 7, 6, 3, 4, 3}
+     */
+
+    private static int maximovector(int[] vector, int indice, int maximo){
+        if(indice == vector.length -1){// caso base
+            if (vector[indice] > maximo){
+
+                maximo = vector[indice];
+            }
+
+
+
+        }else {// llamada ala recursividad
+
+            if (vector[indice] >maximo){
+                maximo = vector[indice];
+            }
+
+            maximo = maximovector(vector, indice +1, maximo);
+
+        }
+
+
+        return maximo;
+
+    }
+
+    /*
+    Metodo auxiliar que lla al metodo recursivo
+     */
+    public  static  int maximoVEctor(int[] vector){
+        return maximovector(vector, 0, 0);
+
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        //System.out.println(Ejercicios.invertirCadena("Recursividad"));
+
+        //int[] vector1={5, 6, 2};
+        //int[] vector2={5, 6, 8};
+        //System.out.println(Ejercicios.compararVectores(vector1, vector2));
+
+        int [] vector = {2, 1, 6, 2, 7, 3, 4, 3};
+        System.out.println(Ejercicios.maximoVEctor(vector));
+    }
+}
